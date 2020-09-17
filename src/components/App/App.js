71eb51036fdaaebe73000089676624095f790d30
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-17 16:58:54 -0500

    axios GET

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    songs: []
+  }
+
+  componentDidMount() {
+    console.log('App is ready');
+    axios({
+      method: 'GET',
+      url: '/songs'
+    }).then(response => {
+      console.log('response:', response);
+      console.log('response.data:', response.data)
+      this.setState({
+        songs: response.data
+      });
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+
   render() {
+    console.log('rendering...');
     return (
       <div className="App">
         <header className="App-header">
@@ -10,6 +33,16 @@ class App extends Component {
         </header>
         <br/>
         <p>Songs go here</p>
+
+        <ul>
+          {this.state.songs.map(song =>
+            <li key={song.id}>{song.track} by {song.artist}</li>
+          )}
+        </ul>
+
+        {/*<pre>
+          {JSON.stringify(this.state.songs, null, 2)}
+        </pre>*/}
       </div>
     );
   }
