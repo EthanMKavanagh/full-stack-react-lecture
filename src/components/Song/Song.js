bcdd67311c6fa25d4844fcc0220aec0b36fc71c4
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-18 13:44:22 -0500

    toggleHidden working

--- src/components/Song/Song.js
+++ src/components/Song/Song.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 
 class Song extends Component {
+
+    state = {
+        isHidden: false
+    }
+
+    toggleHidden = () => {
+        console.log('Hiding song', this.props.track);
+        this.setState({
+            isHidden: !this.state.isHidden
+        });
+    }
+
     render() {
-        console.log('songs props:', this.props)
-        return (
-            <li key={this.props.id}>
-                {this.props.track} by {this.props.artist}
-                <button onClick={this.onHideSong}>Hide Song</button>
-            </li>
-        );
+        console.log('Songs props:', this.props)
+
+        if (this.state.isHidden) {
+            console.log('Hide me');
+            return (
+                <li key={this.props.id}>
+                    <button onClick={this.toggleHidden}>Show Song</button>
+                </li>
+            );
+        }
+        else {
+            console.log('Show me');
+            return (
+                <li key={this.props.id}>
+                    {this.props.track} by {this.props.artist}
+                    <button onClick={this.toggleHidden}>Hide Song</button>
+                </li>
+            );
+        }
     }
 }
 
